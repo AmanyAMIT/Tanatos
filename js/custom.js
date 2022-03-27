fbb29762aefcc761ca8e75b7274b8ef8dd03579c
AmanyAMIT <amany.amit2@gmail.com>
2022-03-27 13:15:55 +0200

    Textarea Modification

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 let sideMenu = document.querySelector('.side-menu'),
     mobilebtn = document.querySelector('.click-menu'),
-    bodyElements = document.getElementsByTagName('body')[0],
     overlayMenu = document.querySelector('.overlayMenu'),
     showmore = document.querySelector('.showhide'),
     input = document.querySelector(".input"),
@@ -8,52 +7,52 @@ let sideMenu = document.querySelector('.side-menu'),
     showless = document.querySelector('.show-less');
 
 
-    function showMore() {
-        var x = document.getElementsByClassName("serviceBox2")
-        var i;
-        for (i = 0; i < x.length; i++) {
-          x[i].classList.toggle("hide");
-          showmore.classList.add("hide");
-          showless.classList.remove("hide");
-        }
-      }
-
-      showless.addEventListener('click',hideShowLess);
-      function hideShowLess(){
-          showless.classList.add("hide");
-          showmore.classList.remove("hide")
-      }
-
-    mobilebtn.addEventListener('click',showMenuOverlay);
-    
-
-    function showMenuOverlay(){
-        sideMenu.classList.add('showMenu');
-        overlayMenu.classList.add('showOverlayBlock');
-        setTimeout(function(){
-            overlayMenu.classList.add('showOverlayOpacity');
-        }, 100);
+function showMore() {
+    var x = document.getElementsByClassName("serviceBox2")
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.toggle("hide");
+        showmore.classList.add("hide");
+        showless.classList.remove("hide");
     }
+}
 
-    overlayMenu.addEventListener('click',removeMenuOverlay);
+showless.addEventListener('click', hideShowLess);
 
-    function removeMenuOverlay(){
-        sideMenu.classList.remove('showMenu');
-        overlayMenu.classList.remove('showOverlayBlock');
-        setTimeout(function(){
-            overlayMenu.classList.remove('showOverlayOpacity');
-        }, 700);
+function hideShowLess() {
+    showless.classList.add("hide");
+    showmore.classList.remove("hide")
+}
+
+mobilebtn.addEventListener('click', showMenuOverlay);
+
+
+function showMenuOverlay() {
+    sideMenu.classList.add('showMenu');
+    overlayMenu.classList.add('showOverlayBlock');
+    setTimeout(function () {
+        overlayMenu.classList.add('showOverlayOpacity');
+    }, 100);
+}
+
+overlayMenu.addEventListener('click', removeMenuOverlay);
+
+function removeMenuOverlay() {
+    sideMenu.classList.remove('showMenu');
+    overlayMenu.classList.remove('showOverlayBlock');
+    setTimeout(function () {
+        overlayMenu.classList.remove('showOverlayOpacity');
+    }, 700);
+}
+
+
+txt.disabled = true;
+input.addEventListener("change", stateHandle);
+
+function stateHandle() {
+    if (document.querySelector(".input").value === "") {
+        txt.disabled = true;
+    } else {
+        txt.disabled = false;
     }
-
-
-    txt.disabled = true;
-    input.addEventListener("change", stateHandle);
-    function stateHandle() {
-        if (document.querySelector(".input").value === "") {
-            txt.disabled = true;
-        } else {
-            txt.disabled = false;
-        }
-    }
-    
-     
+}
